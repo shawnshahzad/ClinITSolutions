a6168ba47910347bdb333d02e3aac8d5b0c0aa2d
shawnshahzad <40645489+shawnshahzad@users.noreply.github.com>
2020-11-25 15:13:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const download = document.querySelector('#file-download');
+download.addEventListener('click', function(){
+    alert("File Downloaded")
+})
+
+
+
+$(".fadeOnLoad").hide(0).delay(500).fadeIn(3000)
+
